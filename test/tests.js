eb271a24f8f9f2dcb47252d6a2877b48e4353728
Mr Eggleton <meggleton@utcsheffield.org.uk>
2015-06-25 09:33:03 +0000

    fixes test folder / mocha compatibility

---
+++ test/tests.js
@@ -0,0 +1,28 @@
+var should = require('should');
+var io = require('socket.io-client'),
+server = require('../app');
+var socketURL = 'http://0.0.0.0:3000';
+var options ={
+transports: ['websocket'],
+'force new connection': true
+};
+
+describe("Lean Server",function(){
+    /* Test 1 - A Single User */
+    it('Should broadcast new user once they connect',function(done){
+        var client = io.connect(socketURL, options);
+        client.on('connect',function(data){
+            client.emit('connection name',chatUser1);
+        });
+        
+        client.on('new user',function(usersName){
+            usersName.should.be.type('string');
+            usersName.should.equal(chatUser1.name + " has joined.");
+            /* If this client doesn't disconnect it will interfere with the next test */
+            client.disconnect();
+            done();
+        });
+    });
+});
+
+

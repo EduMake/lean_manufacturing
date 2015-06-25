39058aaed8518d40936ee675e82969d1d8f2255e
Mr Eggleton <meggleton@utcsheffield.org.uk>
2015-06-25 22:19:58 +0000

    passes first Mocha test

--- socket.js
+++ socket.js
@@ -1,16 +1,41 @@
 module.exports = function(io) {
-
+  
+  var oLean = {
+    aLines:[],
+    getAvailableRoles: function(){
+      if(this.aLines.length === 0) {
+        return [
+          {
+            label:"Line 1 : Manager",
+            line:1,
+            role:"Manager",
+            cellid:0
+          }
+        ];
+      } else {
+          return [];
+      }
+    }
+  }
+  
   io.on('connection', function (socket) {
-    socket.emit('news', { huzza: 'success' });
-    socket.on('my other event', function (data) {
-      console.log(data);
+    console.log("general contection not interested");
+ 
+  
+    var oLeanSocket = io.of('/lean-game');
+    oLeanSocket.on('connection', function(socket){
+      console.log('someone connected');
+      socket.emit('Available Roles', {roles:oLean.getAvailableRoles()});
+      
+      socket.on('Get Available Roles', function (data) {
+        console.log(data);
+        socket.emit('Available Roles', {roles:oLean.getAvailableRoles()});
+      });
+      
+      
+      
+      
     });
   });
   
-/*  var nsp = io.of('/my-namespace');
-  nsp.on('connection', function(socket){
-    console.log('someone connected'):
-  });
-  nsp.emit('hi', 'everyone!');
-  */
 };
--- test/tests.js
+++ test/tests.js
@@ -1,23 +1,38 @@
 var should = require('should');
-var io = require('socket.io-client'),
-server = require('../app');
-var socketURL = 'http://0.0.0.0:3000';
+var io = require('socket.io-client');//,
+//server = require('../app');
+var socketURL = 'http://0.0.0.0:80/lean-game';
 var options ={
-transports: ['websocket'],
-'force new connection': true
+    transports: ['websocket'],
+    'force new connection': true
 };
 
-describe("Lean Server",function(){
-    /* Test 1 - A Single User */
-    it('Should broadcast new user once they connect',function(done){
+describe("Lean Server", function(){
+    
+    it('Should send list of available roles on connection - 1st connection "Line 1 : Manager"',function(done){
         var client = io.connect(socketURL, options);
         client.on('connect',function(data){
-            client.emit('connection name',chatUser1);
+            //client.emit('Get Available Roles');
         });
         
-        client.on('new user',function(usersName){
-            usersName.should.be.type('string');
-            usersName.should.equal(chatUser1.name + " has joined.");
+        client.on('Available Roles',function(oRoles){
+                
+            //console.log("aRoles =", aRoles);
+            oRoles.should.be.type('object');
+            var oExpected = {
+                roles:[
+                    {
+                        label:"Line 1 : Manager",
+                        line:1,
+                        role:"Manager",
+                        cellid:0
+                    }
+                ]
+            };
+            console.log("oRoles    =", oRoles);
+            console.log("oExpected =", oExpected);
+            oRoles.should.eql(oExpected);
+            
             /* If this client doesn't disconnect it will interfere with the next test */
             client.disconnect();
             done();
